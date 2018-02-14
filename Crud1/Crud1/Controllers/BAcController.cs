7f31016f54aa6640c2af69cbb2c3140fbb3f30c8
vijaya.teke <vijaya.teke@IA-PC45>
2018-02-14 14:19:38 +0530

    crud chnages for test the application

---
+++ Crud1/Crud1/Controllers/BAcController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Crud1.Controllers
+{
+    public class BAcController : Controller
+    {
+        // GET: BAc
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
